8299e2e6b20e82fd5958f8e024007ee075cac1da
ChristinaLyu0710 <50678943+ChristinaLyu0710@users.noreply.github.com>
2019-06-20 15:50:23 -0700

    edit according to circle-ci

--- policybot/pkg/storage/spanner/query.go
+++ policybot/pkg/storage/spanner/query.go
@@ -82,12 +82,12 @@ func (s *store) QueryIssuesByRepo(orgID string, repoID string, cb func(*storage.
 func (s *store) QueryTestFlakeByTestName(testName string, cb func(*storage.TestFlake) error) error {
 	iter := s.client.Single().Query(s.ctx, spanner.Statement{SQL: fmt.Sprintf("SELECT * FROM TestFlakes WHERE TestName = '%s'", testName)})
 	err := iter.Do(func(row *spanner.Row) error {
-		issue := &storage.Issue{}
-		if err := row.ToStruct(issue); err != nil {
+		flake := &storage.TestFlake{}
+		if err := row.ToStruct(flake); err != nil {
 			return err
 		}
 
-		if err := cb(issue); err != nil {
+		if err := cb(flake); err != nil {
 			iter.Stop()
 			return err
 		}
