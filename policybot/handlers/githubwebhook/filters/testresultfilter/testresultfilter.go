52457defb3361e9492cd3354f24e2a9f01eb297e
Daniel Tang <tangd@google.com>
2019-08-13 12:48:46 -0700

    move prnum get after error check

--- policybot/handlers/githubwebhook/filters/testresultfilter/testresultfilter.go
+++ policybot/handlers/githubwebhook/filters/testresultfilter/testresultfilter.go
@@ -125,11 +125,11 @@ func (r *TestResultFilter) Handle(context context.Context, event interface{}) {
 
 		sha := p.GetCommit().GetSHA()
 		pr, err := gh.GetPRForSHA(context, r.gc, sha)
-		prNum := int64(pr.GetNumber())
 		if err != nil {
 			scope.Errorf("Error fetching pull request info for commit %s: %v", sha, err)
 			return
 		}
+		prNum := int64(pr.GetNumber())
 		scope.Infof("Commit %s corresponds to pull request %d.", sha, prNum)
 
 		testResults, err := val.trg.CheckTestResultsForPr(context, orgLogin, repoName, prNum)
