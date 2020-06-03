8c702b52393e2510ba864cde01a0e35c71057d9b
heather zheng <sz488@cornell.edu>
2020-06-03 17:41:39 +0000

    remove comment

--- policybot/mgrs/syncmgr/mgr.go
+++ policybot/mgrs/syncmgr/mgr.go
@@ -1227,8 +1227,6 @@ func (ss *syncState) handlePostSubmitTestResults() error {
 			// TODO: this should probably be reported out or something...
 			scope.Warnf("error processing test: %s", e)
 		}).WithParallelism(50).Transform(func(interface{}) (testRunPaths interface{}, err error) {
-			//prNum := prNumi.(string)
-			//tests, err := g.GetTestsForPR(ss.ctx, repo.OrgLogin, repo.RepoName, prNum)
 			tests, err := g.GetPostSubmitTests(ss.ctx, repo.OrgLogin, repo.RepoName)
 			var result [][]string
 
