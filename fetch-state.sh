ca97976bb59c7ab6abe1b0ee3d6547a23bfd58bf
John Howard <howardjohn@google.com>
2020-08-04 13:13:43 -0700

    Zenhub to github projects migration

---
+++ fetch-state.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+TOKEN=${TOKEN:?pass access token as TOKEN}
+
+WORKSPACE=${WORKSPACE:-58d18fe71b06cdd27338c362}
+
+OUT=${OUT:-/tmp/zenhub.csv}
+rm -f ${OUT}
+for repo in $(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/orgs/istio/repos | jq '.[] | (.id | tostring) + "," +.name' -r | grep -v old); do
+  id=$(echo $repo | cut -d, -f1)
+  name=$(echo $repo | cut -d, -f2)
+  echo "Fetching $name/$id"
+  curl -s -H "X-Authentication-Token: ${TOKEN}" -H 'Content-Type: application/json' https://api.zenhub.com/p2/workspaces/${WORKSPACE}/repositories/${id}/board \
+    | jq ".pipelines[]? | \"$REPO,\" + .name+\",\"+(.issues[].issue_number | tostring)" -r \
+    >> ${OUT}
+done
