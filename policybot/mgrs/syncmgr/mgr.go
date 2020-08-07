319730c2b5ab04438a3c85f9521d7e0f01c155d7
Mitch Connors <mitchconnors@gmail.com>
2020-08-06 17:24:12 -0700

    Temporarily disable flake detection (#251)
    * Temporarily disable flake detection
    
    This query is killing the database, which hosts many other functions.  We need to re-evaluate how we do this...
    
    * Update policybot/mgrs/syncmgr/mgr.go

--- policybot/mgrs/syncmgr/mgr.go
+++ policybot/mgrs/syncmgr/mgr.go
@@ -1179,12 +1179,6 @@ func (ss *syncState) handleTestResults() error {
 		if result != nil {
 			return result
 		}
-		// Update cache table to reflect these results.
-		rowCount, err := ss.mgr.store.UpdateFlakeCache(ss.ctx)
-		if err != nil {
-			return err
-		}
-		log.Infof("Updated flake cache with %d additional flakes", rowCount)
 		// TODO: check Post Submit tests as well.
 	}
 
